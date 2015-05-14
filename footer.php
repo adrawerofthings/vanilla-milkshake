4a422fba0e7cf5949fd735049f334286403587bb
Jason Li <jason@hongkonggong.com>
2015-05-14 18:40:02 +0800

    Adding theme link to footer

--- footer.php
+++ footer.php
@@ -14,8 +14,7 @@
 
 	<footer id="colophon" class="site-footer clear-both paddingtop-xlarge" role="contentinfo">
 		<div class="site-info small">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'vanillamilkshake' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'vanillamilkshake' ), 'WordPress' ); ?></a>
-			<a href="<?php echo esc_url( __( 'http://tachyons.io/', 'vanillamilkshake' ) ); ?>"><?php printf( __( ' and %s', 'vanillamilkshake' ), 'Tachyons.' ); ?></a>
+			<a href="<?php echo esc_url( __( 'https://github.com/hongkonggong/vanilla-milkshake', 'vanillamilkshake' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'vanillamilkshake' ), 'a Vanilla Milkshake.' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
