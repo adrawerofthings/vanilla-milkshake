d99abce9a3b0e754ebe936af77f460e0610076d5
Jason Li <jason@hongkonggong.com>
2015-05-26 12:26:15 +0800

    Separating excerpt function out

--- functions.php
+++ functions.php
@@ -147,41 +147,7 @@ require get_template_directory() . '/inc/template-tags.php';
  * Customizer colors.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/** 
- * Allowing HTML (images!) in excerpts
- * | code from: http://aaronrussell.co.uk/legacy/improving-wordpress-the_excerpt/
- * | plus bits and pieces from template-tags.php in 2015 theme
- * | updated bugs by applying: https://web.archive.org/web/20100123185020/http://palehorseinformation.com/2009/12/23/fixing-the-wordpress-excerpt
+/**
+ * Media rich excerpts.
  */
-function improved_trim_excerpt($text) { // Fakes an excerpt if needed
-  global $post;
-  if ( '' == $text ) {
-  	$readmorestring =  __( 'Continue reading', 'vanillamilkshake' );
-    $text = get_the_content($readmorestring);
-    // $text = strip_shortcodes( $text ); have to disable, messes up on <figure>
-    $text = apply_filters('the_content', $text);
-    $text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
-    $text = str_replace('\]\]\>', ']]&gt;', $text);
-    // $text = strip_tags($text, '<p><img><img/><i><em><strong><figure><figcaption><blockquote><a><iframe><sub><sup><pre><code>');
-    $excerpt_length = 72;
-    $words = explode(' ', $text, $excerpt_length + 1);
-    $link = sprintf( '<p class="clear-both"><a href="%1$s" class="more-link">%2$s</a><p>',
-		esc_url( get_permalink( get_the_ID() ) ),
-		/* translators: %s: Name of current post */
-		sprintf( __( $readmorestring . ' %s', 'vanillamilkshake' ), '<span class="screen-reader-text">' . get_the_title( get_the_ID() ) . '</span>' )
-		);
-    if ( (count($words) > $excerpt_length) ) {
-      array_pop($words);
-      $text = implode(' ', $words);
-      $text = $text . '&hellip;';
-      $text = force_balance_tags( $text );
-      $text = $text . $link;
-    }
-  }
-  return $text;
-}
-remove_filter('get_the_excerpt', 'wp_trim_excerpt');
-add_filter('get_the_excerpt', 'improved_trim_excerpt');
-
-
+require get_template_directory() . '/inc/rich-excerpt.php';
